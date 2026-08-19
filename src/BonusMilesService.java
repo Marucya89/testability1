a8c4e61f38fe3f01c1d2c1cd934ef3bbcc9aca60
Maria <marucya.00@mail.com>
2026-08-19 18:52:41 +0300

    gitignore, src added

---
+++ src/BonusMilesService.java
@@ -0,0 +1,14 @@
+public class BonusMilesService {
+    public int calculate( int b ) {
+        int price = 15_536;
+
+        int priceMile = 20;
+        int miles = price / priceMile;
+
+
+        return miles;
+
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 15_536;
+        int miles = service.calculate(price); 
+        System.out.println(miles);
+
+        }
+    }
